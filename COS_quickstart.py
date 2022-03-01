7a0286d24cb11c4e0eb333a5e9883bc96cab029c
Nat Kerman <Nathaniel.Kerman@Gmail.com>
2022-03-01 12:46:38 -0500

    Finished main section on plotting

--- COS_quickstart.py
+++ COS_quickstart.py
@@ -84,11 +84,22 @@ print(f"Corrected TIME-TAG data from segment A in: {corrtag_a}")
 # ### Finally, let's download an example NUV dataset using astroquery in a more condensed form
 
 # %%
-nuv_x1dsum = Path(Observations.download_products(
+nuv_x1dsum_g230l = Path(Observations.download_products(
     Observations.filter_products(
         Observations.get_product_list(
             Observations.query_criteria(
                 obs_id = 'lbbd01020'
+            )
+        ),
+        productSubGroupDescription=["X1DSUM"]
+    )[0]
+)["Local Path"][0])
+
+nuv_x1dsum_g185m = Path(Observations.download_products(
+    Observations.filter_products(
+        Observations.get_product_list(
+            Observations.query_criteria(
+                obs_id = 'LAAD020L0'
             )
         ),
         productSubGroupDescription=["X1DSUM"]
@@ -109,6 +120,12 @@ print(f"This is a {hdr1['EXPTIME']} second exposure on segment {hdr0['SEGMENT']}
 raw_data = Table.read(rawtag_a, hdu=1)
 corr_data = Table.read(corrtag_a, hdu=1)
 
+# %%
+raw_data
+
+# %%
+corr_data
+
 # %% [markdown]
 # ### Let's take a look at how the counts fell on the detector.
 
@@ -122,6 +139,135 @@ plt.xlabel("Detector X Coordinates")
 plt.ylabel("Detector Y Coordinates")
 plt.legend()
 plt.show()
+
+# %% [markdown]
+# The power of this type of "`TIME-TAG`"data is that we have more control and information on each individual photon encounter. Because of this we have the ability to...
+# * Remove suspected noise
+# * Filter out "bad" time intervals
+#   * i.e. to remove data taken when the sun was "up" for HST [see DayNight.ipynb](https://spacetelescope.github.io/COS-Notebooks/DayNight.html)
+# * Split apart our data and examine it at different times
+#   * i.e. for a transit [see SplitTag.ipynb](https://spacetelescope.github.io/COS-Notebooks/SplitTag.html)
+
+# %% [markdown]
+# ### Now let's examine the FUV spectrum
+
+# %%
+for fuv_x1d in fuv_x1dsum_products:
+    hdr0 = fits.getheader(fuv_x1d)
+    hdr1 = fits.getheader(fuv_x1d, ext=1)
+    print(f"{hdr0['ASN_ID']} is a {hdr1['EXPTIME']} second (combined) exposure with the {hdr0['OPT_ELEM']} grating and the {hdr0['CENTRWV']} Å central wavelength setting.")
+
+# %% [markdown]
+# ### We'll look at the structure of the first (G130M) spectrum:
+# * We note there are two rows for each of the segments of the detector
+#   * If we want to plot, we'll need to deal with each of these segments separately
+# * For NUV data, there are three rows for each "Data stripe"
+
+# %%
+fuv_g130m = Table.read(fuv_x1dsum_products[0])
+fuv_g130m
+
+# %%
+# Set up the plot as a single box with size of 10x4 inches, and with a dpi of 100, relevant should we choose to save it:
+fig1, ax = plt.subplots(1,1,figsize=(10,4), dpi = 100)  
+
+# The next few lines are the core of the cell, where we actually place the data onto the plot:
+###############
+# Access each row of data, the longer wvln segment and gets the data we need to plot a spectrum:
+for i, row in enumerate(fuv_g130m):
+    wvln, flux, segment  = row["WAVELENGTH", "FLUX", "SEGMENT"] 
+    ax.plot(wvln, flux, # First two arguments are assumed to be the x-data, y-data
+            linestyle = "-", linewidth = 0.25, c = "km"[i], # These parameters specify the look of the connecting line
+            marker = '.', markersize = 2, markerfacecolor = 'r', markeredgewidth = 0, # The marker parameters specify how the data points will look... 
+                                                                                    # ... if you don't want dots set marker = ''
+            label = segment) # The label is an optional parameter which will allow us to create a legend 
+                            # this label is useful when there are multiple datasets on the same plot
+# The lines after this are all about formatting, adding text, and saving as an image
+###############
+ax.set_title("G130M COS Spectrum", size = 20) # Adds a title of fontsize 20 points
+ax.set_xlabel('Wavelength [$\AA$]', size = 12) # Adds x axis label
+ax.set_ylabel('Flux [$erg\ s^{-1}\ cm^{-2}\ Angstrom^{-1}$]', size = 12) # Adds y label
+
+plt.legend(loc = 'upper right') # Adds a legend with the label specified in the plotting call
+plt.tight_layout() # Trims blank space
+plt.show() # Shows all the plot calls in this cell and "clears" the plotting space - must come after any saving you want to do
+
+# %%
+# Set up the plot as a single box with size of 10x4 inches, and with a dpi of 100, relevant should we choose to save it:
+fig1, ax = plt.subplots(1,1,figsize=(10,4), dpi = 100)  
+
+# The next few lines are the core of the cell, where we actually place the data onto the plot:
+###############
+for j, fuv_file in enumerate(fuv_x1dsum_products):
+    fuv_tab = Table.read(fuv_file)
+    data_label = f"{fits.getval(fuv_file, 'OPT_ELEM')}/{fits.getval(fuv_file, 'CENTRWV'):.0f} "
+    for i, row in enumerate(fuv_tab):
+        wvln, flux, segment  = row["WAVELENGTH", "FLUX", "SEGMENT"] 
+        ax.plot(wvln, flux, # First two arguments are assumed to be the x-data, y-data
+                linestyle = "-", linewidth = 0.5, c = ["bg","mk","yc"][j][i], # These parameters specify the look of the connecting line
+                marker = '', markersize = 2, markeredgewidth = 0, # The marker parameters specify how the data points will look... 
+                label = data_label+segment, alpha=1-0.1*j)
+###############
+ax.set_title("G130M+G160M COS Spectrum", size = 20) # Adds a title of fontsize 20 points
+ax.set_xlabel('Wavelength [$\AA$]', size = 12) # Adds x axis label
+ax.set_ylabel('Flux [$erg\ s^{-1}\ cm^{-2}\ Angstrom^{-1}$]', size = 12) # Adds y label
+
+plt.legend(loc = 'upper right') # Adds a legend with the label specified in the plotting call
+plt.tight_layout() # Trims blank space
+plt.show() # Shows all the plot calls in this cell and "clears" the plotting space - must come after any saving you want to do
+
+# %% [markdown]
+# ### Finally let's look at some NUV spectra
+# Both spectra are of white dwarf stars
+# 1. First the G185M Grating spectrum of GD71
+# 2. Second the G230L Grating spectrum of WD1057+719
+#    1. This is a somewhat strange spectrum COS creates with the G230L grating. The first two stripes are (like all other COS spectra) the first order spectrum of the source. The third stripe is dominated by second order light. It's thus more dispersed, has lower S/N, and falls on top of the first (NUVA) stripe.
+
+# %%
+nuv_tab = Table.read(nuv_x1dsum_g185m)
+# Set up the plot as a single box with size of 10x4 inches, and with a dpi of 100, relevant should we choose to save it:
+fig1, ax = plt.subplots(1,1,figsize=(10,4), dpi = 100)  
+
+# The next few lines are the core of the cell, where we actually place the data onto the plot:
+###############
+# Access each row of data, the longer wvln segment and gets the data we need to plot a spectrum:
+for i, row in enumerate(nuv_tab):
+    wvln, flux, segment  = row["WAVELENGTH", "FLUX", "SEGMENT"] 
+    ax.plot(wvln, flux, # First two arguments are assumed to be the x-data, y-data
+            linestyle = "-", linewidth = 0.25, c = "kmb"[i], # These parameters specify the look of the connecting line
+            marker = '.', markersize = 2, markerfacecolor = 'r', markeredgewidth = 0, # The marker parameters specify how the data points will look... 
+            label = segment)
+###############
+ax.set_title("G185M NUV COS Spectrum", size = 20) # Adds a title of fontsize 20 points
+ax.set_xlabel('Wavelength [$\AA$]', size = 12) # Adds x axis label
+ax.set_ylabel('Flux [$erg\ s^{-1}\ cm^{-2}\ Angstrom^{-1}$]', size = 12) # Adds y label
+
+plt.legend(loc = 'upper right') # Adds a legend with the label specified in the plotting call
+plt.tight_layout() # Trims blank space
+plt.show() # Shows all the plot calls in this cell and "clears" the plotting space - must come after any saving you want to do
+
+# %%
+nuv_tab = Table.read(nuv_x1dsum_g230l)
+# Set up the plot as a single box with size of 10x4 inches, and with a dpi of 100, relevant should we choose to save it:
+fig1, ax = plt.subplots(1,1,figsize=(10,4), dpi = 100)  
+
+# The next few lines are the core of the cell, where we actually place the data onto the plot:
+###############
+# Access each row of data, the longer wvln segment and gets the data we need to plot a spectrum:
+for i, row in enumerate(nuv_tab):
+    wvln, flux, segment  = row["WAVELENGTH", "FLUX", "SEGMENT"] 
+    ax.plot(wvln, flux, # First two arguments are assumed to be the x-data, y-data
+            linestyle = "-", linewidth = 0.25, c = "kmb"[i], # These parameters specify the look of the connecting line
+            marker = '.', markersize = 2, markerfacecolor = (1,0,0,0.5), markeredgewidth = 0, # The marker parameters specify how the data points will look... 
+            label = segment)
+###############
+ax.set_title("G230L NUV COS Spectrum", size = 20) # Adds a title of fontsize 20 points
+ax.set_xlabel('Wavelength [$\AA$]', size = 12) # Adds x axis label
+ax.set_ylabel('Flux [$erg\ s^{-1}\ cm^{-2}\ Angstrom^{-1}$]', size = 12) # Adds y label
+
+plt.legend(loc = 'upper right') # Adds a legend with the label specified in the plotting call
+plt.tight_layout() # Trims blank space
+plt.show() # Shows all the plot calls in this cell and "clears" the plotting space - must come after any saving you want to do
 
 # %%
 
